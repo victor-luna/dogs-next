e60b1635780e1d126835e0e034c62867a23ebc13
Victor Luna <vitor.vazguimaraes@gmail.com>
2024-04-15 14:57:43 -0300

    feat: added header, footer and login page

---
+++ src/components/login/login-form.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import login from '@/actions/login';
+import { useFormState, useFormStatus } from 'react-dom';
+import Button from '@/components/forms/button';
+import Input from '@/components/forms/input';
+import ErrorMessage from '../helper/error-message';
+import React from 'react';
+
+function FormButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled={pending}>Enviando...</Button>
+      ) : (
+        <Button>Entrar</Button>
+      )}
+    </>
+  );
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: '',
+    data: null,
+  });
+
+  React.useEffect(() => {
+    if (state.ok) window.location.href = '/conta';
+  }, [state.ok]);
+
+  return (
+    <>
+      <form action={action}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
+        <FormButton />
+      </form>
+    </>
+  );
+}
