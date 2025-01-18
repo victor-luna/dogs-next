b52f0160e9654d5a8da9f5533363875b048461ab
victor-luna <victor.vazguimaraes@gmail.com>
2025-01-18 18:46:02 -0300

    feat(final-project): create context

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { type_second } from '@/functions/fonts';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { UserContextProvider } from '@/context/user-context';
+import userGet from '@/actions/user-get';
+
+export const metadata: Metadata = {
+  title: 'Dogs Next',
+  description: 'Rede social para cachorros.',
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { data: user } = await userGet();
+
+  return (
+    <html lang="pt-BR">
+      <body className={type_second.variable}>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
+      </body>
+    </html>
+  );
+}
