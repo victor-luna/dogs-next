db246cdd683f61743b6694627374046ab6cca503
Victor Luna <victor.vazguimaraes@gmail.com>
2025-08-08 19:07:10 -0300

    feat(account-feed): implement my account photo feed

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,13 +1,36 @@
 import { Metadata } from 'next'
+import Feed from '../../components/feed/feed'
+import photosGet from '../../actions/photos-get'
+import userGet from '../../actions/user-get'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Minha Conta',
 }
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await userGet()
+  const { data } = await photosGet({ user: user?.username })
   return (
     <main>
-      <h1>Conta</h1>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: '#444', fontSize: '1.25rem', marginBottom: '1rem' }}
+          >
+            Nenhuma foto encontrada.
+          </p>
+          <Link
+            href="/conta/postar"
+            className="button"
+            style={{ display: 'inline-block' }}
+          >
+            Postar foto
+          </Link>
+        </div>
+      )}
     </main>
   )
 }
