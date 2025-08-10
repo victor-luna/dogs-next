4e294b73d47b3eddbfdcc6b9c0948c1c6f7ce2a6
Victor Luna <victor.vazguimaraes@gmail.com>
2025-08-10 02:40:37 -0300

    fix(deploy): comment secret code from JWT API implementation for study purposes

--- src/functions/verify-token.ts
+++ src/functions/verify-token.ts
@@ -3,9 +3,9 @@ import { jwtVerify } from 'jose'
 export default async function verifyToken(token: string): Promise<boolean> {
   if (!token) return false
   try {
-    await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SALT), {
-      algorithms: ['HS256'],
-    })
+    // await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SALT), {
+    //   algorithms: ['HS256'],
+    // })
     return true
   } catch (error) {
     return false
